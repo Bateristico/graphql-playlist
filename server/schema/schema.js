3d5327a89c15c3594a0545c508f03836dc472eb3
Bateristico <camilo.saa@gmail.com>
2021-03-25 21:49:26 +0100

    not null condition added

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,6 +10,7 @@ const {
 	GraphQLID,
 	GraphQLInt,
 	GraphQLList,
+	GraphQLNonNull,
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -95,8 +96,8 @@ const Mutation = new GraphQLObjectType({
 		addAuthor: {
 			type: AuthorType,
 			args: {
-				name: { type: GraphQLString },
-				age: { type: GraphQLInt },
+				name: { type: GraphQLNonNull(GraphQLString) },
+				age: { type: GraphQLNonNull(GraphQLInt) },
 			},
 			resolve(parent, args) {
 				let author = new Author({
@@ -110,9 +111,9 @@ const Mutation = new GraphQLObjectType({
 		addBook: {
 			type: BookType,
 			args: {
-				name: { type: GraphQLString },
-				genre: { type: GraphQLString },
-				authorId: { type: GraphQLID },
+				name: { type: GraphQLNonNull(GraphQLString) },
+				genre: { type: GraphQLNonNull(GraphQLString) },
+				authorId: { type: GraphQLNonNull(GraphQLID) },
 			},
 			resolve(parent, args) {
 				let book = new Book({
