7fbef505b3d7a4ead099cd391d270ff233664222
Bateristico <camilo.saa@gmail.com>
2021-03-25 16:52:45 +0100

    models added

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { DB_URI } = process.env;
 
 const app = express();
+
+// mongodb database connection
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+	console.log('connected to database');
+});
 
 app.use(
 	'/graphql',
